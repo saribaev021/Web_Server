49aab032d3d2f671e7fe86ab76274e94cec1d334
Frodo Barbera <fbarbera@mi-c5.msk.21-school.ru>
2021-03-12 17:34:55 +0300

    time

--- parser_config.cpp
+++ parser_config.cpp
@@ -6,7 +6,7 @@
 /*   By: fbarbera <fbarbera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 14:40:54 by fbarbera          #+#    #+#             */
-/*   Updated: 2021/03/12 16:33:07 by fbarbera         ###   ########.fr       */
+/*   Updated: 2021/03/12 17:34:42 by fbarbera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,39 +24,24 @@ std::vector<t_server_config_data>		parser_config(void)
 }
  
  #include <sys/time.h>
+
+
 std::string Get_local_time()
 {
 	struct timeval tv;
 	time_t t;
-	struct tm *info;
+	struct tm info;
 	char buffer[64];
- 
+
 	gettimeofday(&tv, NULL);
 	t = tv.tv_sec;
-  	info = localtime(&t);
-	strftime (buffer, sizeof buffer, "%a, %d %b %Y %X %Z", info);
+	std::string s = std::to_string(t);
+	strptime(s.c_str(), " %s ", &info);
+	strftime (buffer, sizeof(buffer), "%a, %d %b %Y %X %Z", &info);
 	std::string str = buffer;
 	return str;
 }
 
-std::string f(std::string s)
-{
-	std::string str;
-	int i;
-	for (i = 0; i < s.length(); i++)
-	{
-		if (ft_spases_p(s[i]))
-			break;
-	}
-	while (ft_spases_p(s[i]))
-		i++;
-	while (i < s.length())
-	{
-	 str += s[i];
-		i++;
-	}
-	return str;
-}
 
 int main()
 {
@@ -65,38 +50,38 @@ int main()
 	int j;
 	int k;
 
-	// // std::cout << Get_local_time() << std::endl;
+	std::cout << Get_local_time() << std::endl;
 	// std::cout << gen_auto_page(std::string("/Users/fbarbera/21/"), std::string("21/")) << std::endl;
-	while (i < p.size())
-	{
-		j = 0;
-		std::cout << "_________serv___________"  << std::endl;
-		for (k = 0; k < p[i].server_name.size(); k++)
-			std::cout << p[i].server_name[k] << "\t\t\t server_name" << std::endl;
-		std::cout << p[i].ip << "\t\t\t ip" << std::endl;
-		std::cout << p[i].port << "\t\t\t\t port"<< std::endl;
-		std::cout << p[i].root << "\t\t\t root" << std::endl;
-		std::cout << p[i].max_body_size << "\t\t max_size"<< std::endl;
-		for (k = 0; k < p[i].index_types.size(); k++)
-			std::cout << p[i].index_types[k] << "\t\t\t index" << std::endl;
-		for(std::map<int, std::string>::iterator it = p[i].error_page.begin(); it != p[i].error_page.end(); ++it)
-		{
-			std::cout << it->first << " : " << it->second << std::endl;///вывод на экран
-		}
-		while (j < p[i].location.size())
-		{	
-			std::cout << "_________loc___________" << j  << std::endl;
-			std::cout << p[i].location[j].location << "\t\t\t location" << std::endl;
-			std::cout << p[i].location[j].root << "\t\t\t root" << std::endl;
-			std::cout << p[i].location[j].autoindex << "\t\t\t autoindex"<< std::endl;
-			for (k = 0; k < p[i].location[j].method.size(); k++) 
-				std::cout << p[i].location[j].method[k] << "\t\t\t method "<< std::endl;
-			for (k = 0; k < p[i].location[j].cgi_extensions.size(); k++)
-				std::cout << p[i].location[j].cgi_path[k] << " : " << p[i].location[j].cgi_extensions[k] << "\t\t cji"<< std::endl;
-			std::cout << p[i].location[j].upload_storage << "\t\t\t storage"<< std::endl;
-			j++;
-		}
-		i++;
-	}
+	// while (i < p.size())
+	// {
+	// 	j = 0;
+	// 	std::cout << "_________serv___________"  << std::endl;
+	// 	for (k = 0; k < p[i].server_name.size(); k++)
+	// 		std::cout << p[i].server_name[k] << "\t\t\t server_name" << std::endl;
+	// 	std::cout << p[i].ip << "\t\t\t ip" << std::endl;
+	// 	std::cout << p[i].port << "\t\t\t\t port"<< std::endl;
+	// 	std::cout << p[i].root << "\t\t\t root" << std::endl;
+	// 	std::cout << p[i].max_body_size << "\t\t max_size"<< std::endl;
+	// 	for (k = 0; k < p[i].index_types.size(); k++)
+	// 		std::cout << p[i].index_types[k] << "\t\t\t index" << std::endl;
+	// 	for(std::map<int, std::string>::iterator it = p[i].error_page.begin(); it != p[i].error_page.end(); ++it)
+	// 	{
+	// 		std::cout << it->first << " : " << it->second << std::endl;///вывод на экран
+	// 	}
+	// 	while (j < p[i].location.size())
+	// 	{	
+	// 		std::cout << "_________loc___________" << j  << std::endl;
+	// 		std::cout << p[i].location[j].location << "\t\t\t location" << std::endl;
+	// 		std::cout << p[i].location[j].root << "\t\t\t root" << std::endl;
+	// 		std::cout << p[i].location[j].autoindex << "\t\t\t autoindex"<< std::endl;
+	// 		for (k = 0; k < p[i].location[j].method.size(); k++) 
+	// 			std::cout << p[i].location[j].method[k] << "\t\t\t method "<< std::endl;
+	// 		for (k = 0; k < p[i].location[j].cgi_extensions.size(); k++)
+	// 			std::cout << p[i].location[j].cgi_path[k] << " : " << p[i].location[j].cgi_extensions[k] << "\t\t cji"<< std::endl;
+	// 		std::cout << p[i].location[j].upload_storage << "\t\t\t storage"<< std::endl;
+	// 		j++;
+	// 	}
+	// 	i++;
+	// }
 	return 0;
 }
